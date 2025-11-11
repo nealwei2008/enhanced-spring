24a22e3c01bbf4eb588d988c750243ba113651fe
neal_wei <neal_wei@dayima.com>
2025-11-11 17:16:33 +0800

    升级springboot至3.5.0，以及升级相关依赖

--- src/main/java/com/yoloho/enhanced/spring/support/CustomExceptionHandler.java
+++ src/main/java/com/yoloho/enhanced/spring/support/CustomExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.yoloho.enhanced.spring.support;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
